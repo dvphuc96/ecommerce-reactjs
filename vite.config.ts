c7f195d724c9af232af8518b12e8123892d9e5d9
dvphuc96 <dvphuc96@gmail.com>
2024-07-18 20:31:14 +0700

    setup source

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3005,
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      components: path.resolve(__dirname, "./src/components"),
+      constant: path.resolve(__dirname, "./src/constant"),
+      guards: path.resolve(__dirname, "./src/guards"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      redux: path.resolve(__dirname, "./src/redux"),
+      router: path.resolve(__dirname, "./src/router"),
+      schema: path.resolve(__dirname, "./src/schema"),
+      services: path.resolve(__dirname, "./src/services"),
+      types: path.resolve(__dirname, "./src/types"),
+      utils: path.resolve(__dirname, "./src/utils")
+    },
+  },
+});
